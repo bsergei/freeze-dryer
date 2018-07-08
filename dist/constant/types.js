0148eb3975d100249158fb939653a314a6119027
S <bubenka@gmail.com>
2018-07-08 23:37:23 +0200

    Removed dist

--- dist/constant/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TYPES = {
-    TempSensorService: Symbol.for('TempSensorService'),
-    StorageService: Symbol.for('StorageService')
-};
-exports.default = TYPES;
