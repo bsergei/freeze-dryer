943cb50620220cfd28746f6b1c827003bd1d6445
S <bubenka@gmail.com>
2018-08-26 18:54:17 +0200

    Use post instead of get

--- controller/system.controller.ts
+++ controller/system.controller.ts
@@ -1,11 +1,11 @@
 import {
-    controller, httpGet
+    controller, httpPost
 } from 'inversify-express-utils';
 import * as cp from 'child_process';
 
 @controller('/api/system')
 export class SystemController {
-    @httpGet('/reboot')
+    @httpPost('/reboot')
     public reboot() {
         cp.exec('sudo reboot now');
     }
