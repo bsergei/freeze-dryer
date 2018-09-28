b7ea45cddf41d89994b86c00bc44ee86fe09b6b8
S <bubenka@gmail.com>
2018-09-28 22:03:36 +0200

    Added restart charting api

--- controller/system.controller.ts
+++ controller/system.controller.ts
@@ -9,4 +9,21 @@ export class SystemController {
     public reboot() {
         cp.exec('sudo reboot now');
     }
+
+    @httpPost('/restart-charting')
+    public async rebootCharting() {
+        const p1 = new Promise(r => {
+            cp.exec('sudo systemctl restart influxdb', cb => {
+                r();
+            });
+        });
+
+        const p2 = new Promise(r => {
+            cp.exec('sudo systemctl restart grafana', cb => {
+                r();
+            });
+        });
+
+        return await Promise.all([p1, p2]);
+    }
 }
