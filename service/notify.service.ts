e8b79eb11fd16c45bef01adeea13d156d043fe13
S <bubenka@gmail.com>
2019-03-17 10:56:44 +0100

    Disabled notify service

--- service/notify.service.ts
+++ service/notify.service.ts
@@ -1,6 +1,6 @@
-import * as nodemailer from 'nodemailer';
-import * as smtpTransport from 'nodemailer-smtp-transport';
-import * as emailSettings from '../email.json';
+// import * as nodemailer from 'nodemailer';
+// import * as smtpTransport from 'nodemailer-smtp-transport';
+// import * as emailSettings from '../email.json';
 import { injectable } from 'inversify';
 
 @injectable()
@@ -10,41 +10,41 @@ export class NotifyService {
   }
 
   public error(errors: string[]) {
-    const errorsStr = this.getErrorsString(errors);
-    return new Promise((resolve, reject) => {
-      const transporter = nodemailer.createTransport(
-        smtpTransport((<any>emailSettings).transport));
+    // const errorsStr = this.getErrorsString(errors);
+    // return new Promise((resolve, reject) => {
+    //   const transporter = nodemailer.createTransport(
+    //     smtpTransport((<any>emailSettings).transport));
 
-      const options = (<any>emailSettings).options;
+    //   const options = (<any>emailSettings).options;
 
-      const mailOptions = {
-        from: options.from,
-        to: options.to,
-        subject: options.subject,
-        text: errorsStr
-      };
+    //   const mailOptions = {
+    //     from: options.from,
+    //     to: options.to,
+    //     subject: options.subject,
+    //     text: errorsStr
+    //   };
 
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.log('Error sending email: ' + error, error);
-          reject(error);
-        } else {
-          resolve(info.response);
-        }
-      });
-    });
+    //   transporter.sendMail(mailOptions, (error, info) => {
+    //     if (error) {
+    //       console.log('Error sending email: ' + error, error);
+    //       reject(error);
+    //     } else {
+    //       resolve(info.response);
+    //     }
+    //   });
+    // });
   }
 
-  private getErrorsString(errors: string[]) {
-    if (!errors) {
-      return '';
-    }
-    const dateTime = new Date().toISOString();
-    let errorsMerged = 'Errors happen at time: ' + dateTime + '\n';
-    errorsMerged += '\n-------------------------\n';
-    for (const err of errors) {
-      errorsMerged += err + '\n-------------------------\n';
-    }
-    return errorsMerged;
-  }
+  // private getErrorsString(errors: string[]) {
+  //   if (!errors) {
+  //     return '';
+  //   }
+  //   const dateTime = new Date().toISOString();
+  //   let errorsMerged = 'Errors happen at time: ' + dateTime + '\n';
+  //   errorsMerged += '\n-------------------------\n';
+  //   for (const err of errors) {
+  //     errorsMerged += err + '\n-------------------------\n';
+  //   }
+  //   return errorsMerged;
+  // }
 }
